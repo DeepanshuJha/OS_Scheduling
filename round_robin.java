298e6b24f108acdc4ca7d090b93725d444387731
dsilent <deeps24jha@gmail.com>
2019-04-15 15:38:20 +0530

    second commit round_robin completed

--- round_robin.java
+++ round_robin.java
@@ -1,4 +1,3 @@
-import java.awt.SystemTray;
 import java.util.*;
 
 public class round_robin{
@@ -7,17 +6,21 @@ public class round_robin{
     Queue<Integer> q;
     HashMap<Integer, Integer> h = new HashMap<>();
     int n_process, quantum;
-    int process[], at[], bt[];
+    int process[], at[], bt[], bt0[];
     int et[], tat[], wt[];
     int process_sequence[];
     int process_cnt;
     int time;
+    float avg_wt;
+    float avg_tat;
 
     round_robin(){
         sc = new Scanner(System.in);
         n_process = 0;
         quantum = 0;
         process_cnt = 0;
+        avg_wt = 0.0f;
+        avg_tat = 0.0f;
         q = new LinkedList<>();
     }
 
@@ -32,6 +35,7 @@ public class round_robin{
         process = new int[n_process];
         at = new int[n_process];
         bt = new int[n_process];
+        bt0 = new int[n_process];
         et = new int[n_process];
         tat = new int[n_process];
         wt = new int[n_process];
@@ -50,20 +54,23 @@ public class round_robin{
         System.out.print("Enter BT : ");
         for(int i = 0; i < n_process; i++){
             bt[i] = sc.nextInt();
+            bt0[i] = bt[i];
         }
     }
 
     void findAvgTime(){
         time = Integer.MAX_VALUE;
+        int start_pid = -1;
         for(int i = 0; i < n_process; i++){
             if(at[i] < time){
                 time = at[i];
+                start_pid = i;
             }
             h.put(i, 0);
         }
         System.out.println("Start Time : " + time);
-        q.add(0); //start process id
-        h.put(0, 1);
+        q.add(start_pid); //start process id
+        h.put(start_pid, 1);
         while(q.size() != 0){
             System.out.println("Queue : " + q);
             int pid = q.remove();
@@ -72,10 +79,14 @@ public class round_robin{
                 time += bt[pid];
                 et[pid] = time;
                 bt[pid] -= bt[pid];
+                process_sequence[process_cnt] = pid;
+                process_cnt++;
             }else{
                 time += quantum;
                 et[pid] = time;
                 bt[pid] -= quantum; 
+                process_sequence[process_cnt] = pid;
+                process_cnt++;
             }
             for(int i = 0; i < n_process; i++){
                 if(h.containsKey(i)){
@@ -93,14 +104,33 @@ public class round_robin{
                 h.put(pid, 1);
             }
         }
+
+        for(int i = 0; i < n_process; i++){
+            tat[i] = et[i] - at[i];
+            wt[i] = tat[i] - bt0[i];
+            avg_wt += wt[i];
+            avg_tat += tat[i];
+        }
+
+        avg_tat /= n_process;
+        avg_wt /= n_process;
     }
 
     void output(){
         System.out.println("==================OUTPUT==================");
         System.out.println("PID\tAT\tBT\tCT\tTAT\tWT");
         for(int i = 0; i < n_process; i++){
-            System.out.println(process[i]+"\t"+at[i]+"\t"+bt[i]+"\t"+et[i]);
+            System.out.println(process[i]+"\t"+at[i]+"\t"+bt0[i]+"\t"+et[i]+"\t"+tat[i]+"\t"+wt[i]);
         }
+
+        System.out.println("\nAverage WT : " + avg_wt);
+        System.out.println("Average TAT : " + avg_tat);
+
+        System.out.print("Process Sequence : ");
+        for(int i = 0; i < process_cnt - 1; i++){
+            System.out.print(process_sequence[i]+" -> ");
+        }
+        System.out.println(process_sequence[process_cnt - 1]);
     }   
 
     public static void main(String args[]){
@@ -113,11 +143,11 @@ public class round_robin{
 
 /*
 
-4
-2
-0 1 2 3
-0 1 2 4
-5 4 2 1
+n_process = 4
+quantum = 2
+pids = 0 1 2 3
+at[] = 0 1 2 4
+bt[] = 5 4 2 1
 
 
 */
